13e91a1b6bba41953de14e43b79f8496a7e26773
James Douglass <jamesdouglassusa@gmail.com>
2024-02-06 22:03:27 -0800

    Reworking test shell script.

--- test.sh
+++ test.sh
@@ -6,20 +6,30 @@ CPU_DIR="cpu-results"
 mkdir "$CPU_DIR" || echo "$CPU_DIR/ already exists"
 for i in {0..5}
 do
-    sysbench cpu > $CPU_DIR/cpu-$i.txt
+    sysbench cpu run > $CPU_DIR/cpu-$i.txt
 done;
 
 FILEIO_DIR="fileio-results"
 mkdir $FILEIO_DIR || echo "$FILEIO_DIR/ already exists"
+pushd $FILEIO_DIR
 sysbench fileio prepare
-for option in "seqwr seqrewr seqrd rndrd rndwr rndrw"
+options=(
+    "seqwr"
+    "seqrewr"
+    "seqrd"
+    "rndrd"
+    "rndwr"
+    "rndrw"
+)
+for option in "${options[@]}"
 do
     for i in {0..5}
     do
-        sysbench fileio run --file-test-mode=$option > $FILEIO_DIR/fileio-$option-$i.txt
+        sysbench fileio run --file-test-mode=$option > "fileio-$option-$i.txt"
     done
 done
 sysbench fileio cleanup
+popd
 
 # run an InVEST model - scenic quality
 wget https://storage.googleapis.com/releases.naturalcapitalproject.org/invest/3.14.1/data/ScenicQuality.zip
