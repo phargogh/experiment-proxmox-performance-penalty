4f706157a06a332e9cf4d8ba149036b92ea0611e
James Douglass <jamesdouglassusa@gmail.com>
2024-02-06 21:50:28 -0800

    Adding a test InVEST script

---
+++ test-sq.py
@@ -0,0 +1,16 @@
+import logging
+import os
+import sys
+
+import natcap.invest
+from natcap.invest import datastack
+from natcap.invest.scenic_quality import scenic_quality
+
+if not os.path.exists(sys.argv[1]):
+    os.makedirs(sys.argv[1])
+
+logging.basicConfig(level=logging.INFO, filename=os.path.join(sys.argv[1], 'logfile.txt'))
+
+args = datastack.extract_parameter_set('/cwd/ScenicQuality/scenic_quality_wcvi_sample.invest.json').args
+args['workspace_dir'] = sys.argv[1],
+scenic_quality.execute(args)
