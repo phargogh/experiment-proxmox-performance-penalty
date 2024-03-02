49090a1e73736a807776b8f1c8e8f77a9a5eb2ca
James Douglass <jamesdouglassusa@gmail.com>
2024-03-01 22:34:17 -0800

    Update test-sq.py
    Forgot to remove a comma

--- test-sq.py
+++ test-sq.py
@@ -12,5 +12,5 @@ if not os.path.exists(sys.argv[1]):
 logging.basicConfig(level=logging.INFO, filename=os.path.join(sys.argv[1], 'logfile.txt'))
 
 args = datastack.extract_parameter_set('/natcap/ScenicQuality/scenic_quality_wcvi_sample.invest.json').args
-args['workspace_dir'] = sys.argv[1],
+args['workspace_dir'] = sys.argv[1]
 scenic_quality.execute(args)
